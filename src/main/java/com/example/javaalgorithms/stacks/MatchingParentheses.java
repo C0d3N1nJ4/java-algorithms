762e7f3627c0b6f7948e1943ae9c7461aef9f284
naiomi <Naiomi.Naidoo@icloud.com>
2023-07-21 15:34:27 +0200

    Stack algorithm - Find matching parentheses

---
+++ src/main/java/com/example/javaalgorithms/stacks/MatchingParentheses.java
@@ -0,0 +1,33 @@
+package com.example.javaalgorithms.stacks;
+
+import java.util.Stack;
+
+public class MatchingParentheses {
+
+    public static boolean hasMatchingParentheses(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(hasMatchingParentheses("((()))")); //true
+        System.out.println(hasMatchingParentheses("")); //true
+        System.out.println(hasMatchingParentheses("()")); //true
+        System.out.println(hasMatchingParentheses("((algorithm)))")); //false
+        System.out.println(hasMatchingParentheses("((())")); //false
+        System.out.println(hasMatchingParentheses("((()))))")); //false
+
+    }
+}
