8909b2a706ddb8c8d7993ec1f7bc4361493e9e8c
naiomi <Naiomi.Naidoo@icloud.com>
2023-07-31 21:11:50 +0200

    Count frequency of element in unsorted array

---
+++ src/main/java/com/example/javaalgorithms/hash/CountElementsInUnsortedArray.java
@@ -0,0 +1,26 @@
+package com.example.javaalgorithms.hash;
+
+import java.util.HashMap;
+
+public class CountElementsInUnsortedArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 1, 3, 6, 6, 1, 1, 1, 1, 1, 1};
+        System.out.println(countElements(arr, 1));
+        System.out.println(countElements(arr, 2));
+        System.out.println(countElements(arr, 3));
+        System.out.println(countElements(arr, 6));
+    }
+
+    public static int countElements(int[] arr, int item) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int x : arr) {
+            if (map.containsKey(x)) {
+                map.put(x, map.get(x) + 1);
+            } else {
+                map.put(x, 1);
+            }
+        }
+        return map.get(item);
+    }
+}
